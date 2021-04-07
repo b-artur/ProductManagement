d92fc7520ca8ae0eb038651831b44d3345d0a75f
Artur Bakhmanov <b-artur@yandex.ru>
2021-04-07 22:44:49 +0300

    Module 5 : Improved Class Design

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -9,6 +9,7 @@ package labs.pm.data;
 import java.math.BigDecimal;
 
 import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.data.Rating.*;
 
 /**
  * {@code Product} class represents properties and behaviors of
@@ -35,31 +36,47 @@ public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-//        price = BigDecimal.ONE;
-        this.price = price;
-    }
+//    public void setPrice(final BigDecimal price) {
+////        price = BigDecimal.ONE;
+//        this.price = price;
+//    }
 
     /**
      * Calculates discount based on a product price and
@@ -69,5 +86,17 @@ public class Product {
      */
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(this.id, this.name, this.price, newRating);
+        /*
+         *  Using this. is optional, cos method is inside the Product class,
+         * so it can directly access any of its private variables or methods
+         */
     }
 }
---
+++ src/labs/pm/data/Rating.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+ * Copyright (c) 2021.                                                        *
+ * This code is created on educational purposes and for sure is strongly      *
+ * protected and something else about something else                          *
+ ******************************************************************************/
+
+package labs.pm.data;
+
+/**
+ * @author Artur Bakhmanov
+ */
+public enum Rating {
+    NOT_RATED("\u2606\u2606\u2606\u2606\u2606"),
+    ONE_STAR("\u2605\u2606\u2606\u2606\u2606"),
+    TWO_STAR("\u2605\u2605\u2606\u2606\u2606"),
+    THREE_STAR("\u2605\u2605\u2605\u2606\u2606"),
+    FOUR_STAR("\u2605\u2605\u2605\u2605\u2606"),
+    FIVE_STAR("\u2605\u2605\u2605\u2605\u2605");
+    private String stars;
+
+    Rating(String stars) {
+        this.stars = stars;
+    }
+
+    public String getStars() {
+        return stars;
+    }
+}
